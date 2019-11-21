93a339cc2218fc3083b70a31e89eb889429d5e7f
Aldi Maulana <aldi@femaledaily.com>
2019-11-22 05:03:03 +0700

    added: binding and view changed

--- src/main/java/com/ismealdi/catering/CateringActivity.kt
+++ src/main/java/com/ismealdi/catering/CateringActivity.kt
@@ -1,8 +1,13 @@
 package com.ismealdi.catering
 
 import android.os.Bundle
+import com.ismealdi.catering.databinding.ViewCateringBinding
 import com.ismealdi.meepopup.base.AmActivity
 
-class CateringActivity : AmActivity(R.layout.activity_catering) {
+class CateringActivity : AmActivity<ViewCateringBinding>(R.layout.view_catering) {
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
 
 }
