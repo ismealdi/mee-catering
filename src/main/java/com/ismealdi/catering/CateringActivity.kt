01a4ded0af2a0951ac7bd17f7ee3a9780c25a7b6
Aldi Maulana <aldi@femaledaily.com>
2019-12-12 15:54:54 +0700

    changed: layout adjuster and gradle changed

--- src/main/java/com/ismealdi/catering/CateringActivity.kt
+++ src/main/java/com/ismealdi/catering/CateringActivity.kt
@@ -1,10 +1,9 @@
 package com.ismealdi.catering
 
 import android.os.Bundle
-import com.ismealdi.catering.databinding.ViewCateringBinding
 import com.ismealdi.meepopup.base.AmActivity
 
-class CateringActivity : AmActivity<ViewCateringBinding>(R.layout.view_catering) {
+class CateringActivity : AmActivity(R.layout.view_catering) {
 
     override fun initView(savedInstanceState: Bundle?) {
 
